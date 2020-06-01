a0a6faa9d263429075763109ef270660b83f5bc7
mdsatria <mdsatria@gmail.com>
2020-06-01 12:49:36 +0800

    commit

--- build/build.py
+++ build/build.py
@@ -1,7 +1,5 @@
 import numpy as np
-import keras
 from keras.datasets import mnist
-
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 reshaped_x_train = x_train.reshape(-1,784).astype(np.float32)
@@ -23,3 +21,6 @@ model.add(Dense(10, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(reshaped_x_train, reshaped_y_train, validation_data=(reshaped_x_test, reshaped_y_test), epochs=10)
 model.save('model.h5')
+
+
+
